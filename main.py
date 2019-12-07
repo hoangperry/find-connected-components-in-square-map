e87b4d889cae0eee5a70a3e7f3a71534b27fa361
hoangperry <prodai9573@gmail.com>
2019-12-07 18:44:10 +0700

    add click feature

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class SquareMap:
         pygame.init()
         self.board_width = board_width
         self.board_height = board_height
+        self.board = [[0 for i in range(board_width)] for i in range(board_height)]
         self.cellsize = cellsize
         self.windows_width = cellsize * board_width
         self.windows_height = cellsize * board_height
@@ -23,24 +24,40 @@ class SquareMap:
         for i in range(self.board_width):
             for j in range(self.board_height):
                 if (i + j) % 2 == 0:
-                    background_color = self.game_color['gray']
+                    if self.board[j][i] == 0:
+                        background_color = self.game_color['gray']
+                    else:
+                        background_color = self.game_color['red']
+
                 else:
-                    background_color = self.game_color['white']
+                    if self.board[j][i] == 0:
+                        background_color = self.game_color['white']
+                    else:
+                        background_color = self.game_color['dark_red']
                 pygame.draw.rect(self.game_display, background_color,
                                  [i * self.cellsize, j * self.cellsize,
                                   self.cellsize, self.cellsize])
+
+    def update_pos(self, pos):
+        change_x_pos = pos[0]//self.cellsize
+        change_y_pos = pos[1]//self.cellsize
+        self.board[change_y_pos][change_x_pos] = 0 if self.board[change_y_pos][change_x_pos] == 1 else 1
+        print(self.board[change_y_pos][change_x_pos])
 
     def loop(self):
         crashed = False
         while not crashed:
             for event in pygame.event.get():
-                print(event)
+                # print(event)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.update_pos(pygame.mouse.get_pos())
                 if event.type == pygame.QUIT:
                     crashed = True
+
             self.game_display.fill(self.game_color['white'])
             self.display_map()
+            # self.display_num()
             pygame.display.update()
-            self.clock.tick(60)
 
 
 if __name__ == "__main__":
